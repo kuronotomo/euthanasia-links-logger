5ca610851c2ea537340080a853bf455f1e104e20
kuronotomo <kuronotomo1@gmai.com>
2022-10-07 06:33:39 +0900

    ログの内容をコードブロックで表示する

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const linksProjName = 'euthanasia-links';
 const logsProjName = 'euthanasia-links-logs';
 
 const TOTAL_CNT = 'ページ総数';
-const ADDITION_CNT = '追加されたページ数';
-const APPROVAL_CNT = '承認されたページ数';
+const ADDITION_CNT = '前回以降に追加されたページ数';
+const APPROVAL_CNT = '前回以降に承認されたページ数';
 const UNAPPROVAL_CNT = '未承認のページ数';
 
 const i = ' '; // Scrapboxでインデントを示す文字
-
-const addBackquote = (text) => `\`${text}\``;
-const removeBackquote = (text) => text.split('').filter(char => char !== '\`').join('');
 
 // 前回のログ
 const prevLogs = await fetch(`${apiRoot}/pages/${logsProjName}/?limit=1`)
@@ -21,7 +18,7 @@ const prevLogs = await fetch(`${apiRoot}/pages/${logsProjName}/?limit=1`)
 const pageList = await fetch(`${apiRoot}/pages/${linksProjName}/?limit=1`)
   .then(res => res.json());
 
-const body = [];
+const body = ['code: log.txt'];
 const errors = [];
 
 if (prevLogs.length > 0) {
@@ -31,23 +28,23 @@ if (prevLogs.length > 0) {
   }
 
   const prevTotalCnt = Boolean(found)
-    ? parseInt(removeBackquote(found).split(`${TOTAL_CNT}: `)[1])
+    ? parseInt(found.split(`${TOTAL_CNT}: `)[1])
     : 0;
   const additionCnt = pageList.count - prevTotalCnt;
   
   body.push(
-    `${i}${addBackquote(`${TOTAL_CNT}: ${pageList.count}`)}`,
-    `${i}${addBackquote(`${ADDITION_CNT}: ${additionCnt}`)}`,
-    `${i}${addBackquote(`${APPROVAL_CNT}: 0`)}`,
-    `${i}${addBackquote(`${UNAPPROVAL_CNT}: 0`)}`,
+    `${i}${`${TOTAL_CNT}: ${pageList.count}`}`,
+    `${i}${`${ADDITION_CNT}: ${additionCnt}`}`,
+    `${i}${`${APPROVAL_CNT}: 0`}`,
+    `${i}${`${UNAPPROVAL_CNT}: 0`}`,
   );
 
 } else {
   body.push(
-    `${i}${addBackquote(`${TOTAL_CNT}: ${pageList.count}`)}`,
-    `${i}${addBackquote(`${ADDITION_CNT}: ${pageList.count}`)}`,
-    `${i}${addBackquote(`${APPROVAL_CNT}: 0`)}`,
-    `${i}${addBackquote(`${UNAPPROVAL_CNT}: 0`)}`,
+    `${i}${`${TOTAL_CNT}: ${pageList.count}`}`,
+    `${i}${`${ADDITION_CNT}: ${pageList.count}`}`,
+    `${i}${`${APPROVAL_CNT}: 0`}`,
+    `${i}${`${UNAPPROVAL_CNT}: 0`}`,
   );
 }
 
